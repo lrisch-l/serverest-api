91cb20fa2054ed6aa6a07986f026c1029816598c
Leandro Risch <leandrorisch4@gmail.com>
2025-09-29 19:18:26 -0300

    Install Mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,20 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   video: false,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     baseUrl: 'https://serverest.dev',
     supportFile: 'cypress/support/e2e.js',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    setupNodeEvents(_on, config) {
-      return config
+    setupNodeEvents(on, config) {
+      return config;
     }
   }
-})
+});
