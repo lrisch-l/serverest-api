56dfe2c51f484007dea7982c7696d0fa8f1634ed
Leandro Risch <leandrorisch4@gmail.com>
2025-09-30 17:26:47 -0300

    Configure Cypress to clean reports and disable video for API tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,25 +1,26 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  video: true, // Enable video recording for each test run
-  reporter: 'cypress-mochawesome-reporter', // Use Mochawesome reporter with embedded screenshots
+  video: false,
+  reporter: "cypress-mochawesome-reporter", // Use Mochawesome reporter with embedded screenshots
   reporterOptions: {
-    reportDir: 'cypress/reports', // Directory to save the report
-    reportPageTitle: 'Serverest API Test Report', // Custom title for the HTML report
-    embeddedScreenshots: true, // Embed screenshots directly into the report
-    inlineAssets: true, // Include styles and scripts inline for standalone viewing
-    charts: true, // Display charts for test statistics
-    overwrite: false, // Do not overwrite previous reports
-    html: true, // Generate HTML report
-    json: true // Generate JSON report
+    reportDir: "cypress/reports",
+    reportFilename: "index",
+    overwrite: true,
+    html: true,
+    json: true,
+    inlineAssets: true,
+    charts: true,
+    embeddedScreenshots: true,
+    reportPageTitle: "Serverest API Test Report",
   },
   e2e: {
-    baseUrl: 'https://serverest.dev', // Base URL for all tests
-    supportFile: 'cypress/support/e2e.js', // Path to support file
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Pattern to locate test files
+    baseUrl: "https://serverest.dev", // Base URL for all tests
+    supportFile: "cypress/support/e2e.js", // Path to support file
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // Pattern to locate test files
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on); // Register Mochawesome plugin
+      require("cypress-mochawesome-reporter/plugin")(on); // Register Mochawesome plugin
       return config;
-    }
-  }
+    },
+  },
 });
